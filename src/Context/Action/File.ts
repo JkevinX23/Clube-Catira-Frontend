0d5c983af59b63053c620872903f220c906c991a
jkevinx23 <kevingomesr@gmail.com>
2021-05-21 23:10:58 -0300

    Test: Log Mobile - Change FileType

--- src/Context/Action/File.ts
+++ src/Context/Action/File.ts
@@ -2,4 +2,6 @@ import api from 'services/api'
 import { GetFileProps } from 'Types'
 
 export const postFile = async (data: FormData) =>
-  api.post<GetFileProps>('files', data)
+  api.post<GetFileProps>('files', data, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
